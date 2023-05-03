62decb0239b7e880d0120711ad73ab1289157e2a
salewis38 <46199139+salewis38@users.noreply.github.com>
2023-05-03 23:21:15 +0100

    Fixed midnight rollover issue in SoC calcs

--- palm.py
+++ palm.py
@@ -49,8 +49,9 @@ import settings as stgs
 # v0.8.4b   24/Dec/22 Winter boost conditional on CO2 intensity
 # v0.8.4c   01/Jan/23 General tidy up
 # v0.8.4d   09/Jan/23 Updated GivEnergyObj to download & validate inverter commands
+# v0.8.5    04/May/23 Fixed midnight rollover issue in SoC calculation timing 
 
-PALM_VERSION = "v0.8.4d"
+PALM_VERSION = "v0.8.5"
 # -*- coding: utf-8 -*-
 
 class GivEnergyObj:
@@ -957,8 +958,13 @@ if __name__ == '__main__':
         else:
             # Schedule activities at specific intervals
 
+            start_time_mins = time_to_mins(stgs.GE.start_time)
+            if start_time_mins < 6:  # Correct for off-peak start = 00:00
+                start_time_mins = start_time_mins + 1440
+
             # 5 minutes before off-peak start for next day's forecast
-            if TIME_NOW_MINS_VAR == time_to_mins(stgs.GE.start_time) - 5:
+            if (TEST_MODE and LOOP_COUNTER_VAR == 0) or \
+                TIME_NOW_MINS_VAR == start_time - 5:
                 try:
                     solcast.update()
                 except Exception:
@@ -966,7 +972,7 @@ if __name__ == '__main__':
 
             # 2 minutes before off-peak start for setting overnight battery charging target
             if (TEST_MODE and LOOP_COUNTER_VAR == 2) or \
-                TIME_NOW_MINS_VAR == time_to_mins(stgs.GE.start_time) - 2:
+                TIME_NOW_MINS_VAR == start_time - 2:
                 # compute & set SoC target
                 try:
                     ge.get_load_hist()
@@ -984,7 +990,8 @@ if __name__ == '__main__':
                 env_obj.reset_sr_ss()
 
             # Afternoon battery boost in winter months to load shift from peak period
-            if MONTH_VAR in stgs.GE.winter:
+            if MONTH_VAR in stgs.GE.winter and \
+                time_to_mins(stgs.GE.boost_start) < time_to_mins(stgs.GE.boost_finish):
                 if TIME_NOW_MINS_VAR == time_to_mins(stgs.GE.boost_start) and env_obj.co2_high:
                     print("info; Enabling afternoon battery boost")
                     ge.set_mode("charge_now")
